97debe9b90e3408f5700e2f2dbf3a8fb68651052
chali <572923574@qq.com>
2018-10-30 23:21:42 +0800

    account.vue添加表格

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <el-col :span="3">
+            <Menu></Menu>
+        </el-col>
+        <el-col :span="21">
+            <router-view></router-view>
+        </el-col>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+import Menu from "@/views/menu/Menu.vue";
+
+export default {
+  name: "app",
+  components: {
+    Menu
+  }
+};
+</script>
+
 <style lang="less">
+html,body {
+  height: 100%;
+  overflow: hidden;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  height: 100%;
+  font-size: 12px;
+  >div{
+    height: 100%;
   }
+}
+* {
+  margin: 0px;
+  padding: 0px;
 }
 </style>
