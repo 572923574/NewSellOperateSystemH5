f08fd6d61abe18333ff46a79260f3febdb398278
chali <572923574@qq.com>
2018-10-30 23:24:30 +0800

    git 推送account.vue

---
+++ src/views/accountSet/Account.vue
@@ -0,0 +1,104 @@
+<template>
+    <el-table :data="tableData3" style="width: 100%" height="250" border>
+        <el-table-column v-for="(headerItem,key) in tabHeader" :fixed="headerItem.fixed" :label="headerItem.label" :prop="headerItem.prop" :width="headerItem.width" :key="key" ></el-table-column>
+    </el-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabHeader: [
+        {
+          label: "名称",
+          width: "150",
+          prop: "name",
+          fixed: true
+        },
+        {
+          label: "手机",
+          width: "150",
+          prop: "mobile",
+          fixed: true
+        },
+        {
+          label: "角色",
+          width: "150",
+          prop: "role",
+          fixed: true
+        },
+        {
+          label: "加入日期",
+          width: "150",
+          prop: "date",
+          fixed: true
+        },
+        {
+          label: "状态",
+          width: "150",
+          prop: "status",
+          fixed: true
+        }
+      ],
+      tableData3: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-02",
+          name: "chali",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-04",
+          name: "xl",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎1",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎2",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎3",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎4",
+          mobile: "18123456789",
+          status: "0",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/views/menu/Menu.vue
@@ -0,0 +1,140 @@
+<template>
+    <!-- //左侧菜单 -->
+    <div  class="leftMenu">
+        <div class="shopAndAccount">
+        <!-- //店名与账号 -->
+            <div class="shopImg"></div>
+            <!-- 店铺图片 -->
+            <div class="nameDiv">
+                <!-- 店铺名称 -->
+                <div class="shopName">{{shopName}}</div>
+                <div class="accountDiv">
+                    <!-- 账号名称 -->
+                    <div class="accountName">{{accountName}}</div>
+                    
+                </div>
+            </div>
+            <!-- 推出登陆 -->
+            <div class="exitBtn">{{exitText}}</div>
+        </div>
+        <el-menu
+            default-active="2"
+            class="leftMenuDiv"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>{{menuOneItem}}</span>
+                </template>
+                <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group>
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+                <i class="el-icon-document"></i>
+                <span slot="title">库存设置</span>
+            </el-menu-item>
+            <el-menu-item index="4" @click="changePage('/')">
+                <i class="el-icon-setting"></i>
+                <span slot="title">账号设置</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuOneItem: "系统设置",
+      shopName:"微信商城分销系统",
+      accountName:"分销达人",
+      exitText:"退出"
+    };
+  },
+  methods: {
+    handleOpen() {},
+    changePage(path){debugger
+        this.$router.push({path:path});
+    },
+    handleClose() {}
+  }
+};
+</script>
+<style lang="less" scoped>
+.leftMenu {
+    height: 100%;
+    background-color: rgb(84, 92, 100);
+    .shopAndAccount{
+        height: 40px;
+        padding: 10px;
+        max-width: calc(100% - 20px);
+        color: gold;
+        .shopImg{
+            width:40px;
+            height: 40px;
+            background-color:gold;
+            float: left;
+        }
+        .nameDiv{
+            float: left;
+            padding-left:10px; 
+            max-width: calc(100% - 90px);
+            .shopName{
+                height: 20px;
+                line-height: 20px;
+                text-align: left;
+            }
+            .accountDiv{
+                height: 20px;
+                .accountName{
+                    float: left;
+                    max-width: calc(100% - 20px);
+                }
+            }
+        }
+        .exitBtn{
+            float: right;
+            width: 40px;
+            text-align: right;
+            padding-right:10px;
+            height: 100%;
+            line-height: 40px;
+            font-size: 14px;
+        }
+    }
+    .leftMenuDiv{
+        text-align: left;
+        border-right: none;
+        width: 100%;  
+        .el-submenu__title {
+            // text-align: left;
+        }
+        .el-submenu {
+            // text-align: left;
+            .el-menu-item {
+                min-width: auto;  
+            }
+        }
+        .el-menu-item {
+            // text-align: left;
+        }
+    }
+}
+</style>
